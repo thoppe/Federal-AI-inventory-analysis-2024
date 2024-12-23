face6e16d3d7dfe3f42001eabf3eca1a6a542ec7
thoppe <travis.hoppe@gmail.com>
2024-12-23 14:20:02 -0500

    Update project cards

--- src/P5_build_project_cards.py
+++ src/P5_build_project_cards.py
@@ -11,18 +11,42 @@ with open(f_schema) as stream:
     schema = pd.DataFrame(schema).set_index("name")
 
 
-def compute(f0, f1):
-    row = df.loc[f0]
+def compute(use_case_ID, f1):
+    row = df.loc[use_case_ID]
 
     output = []
-    output.append(f"## {f0}")
+    title = row['2_use_case_name']
+    agency = row['3_agency']
+    agency_abbr = row['3_abr']
+    subagency = row['4_bureau']
+    
+    output.append(f"# {title}")
+    output.append(f"## {use_case_ID}")
+    output.append(f"_{agency}_ ({agency_abbr}) / {subagency}")
+    skip_rows = ['2_use_case_name', '3_abr', '3_agency', '4_bureau']
+    
     output.append("\n")
+    
 
     for k, v in row.items():
         # k = ' '.join(k.split('_')[1:]).title()
-        name = schema.loc[k]["description"]
-        name = name.strip().strip(".") + ":"
 
+        requirements = schema.loc[k]['constraints']
+
+        if not v:
+            continue
+            if isinstance(requirements, dict) and 'required' not in requirements:
+                continue
+                #if not requirements['required']:
+                #   continue
+
+        if k == "47_timely_resources":
+            name = "Has the communication been timely"
+        elif k in skip_rows:
+            continue
+        else:
+            name = schema.loc[k]["description"]
+            name = name.strip().strip(".")
         output.append(f"+ **{name}**: {v}")
 
         # Do something special if there is an ENUM
@@ -35,5 +59,5 @@ def compute(f0, f1):
 
 key = "1_use_case_id"
 df = df.set_index(key).fillna("")
-Pipe(df.index, "data/project_cards/individual", output_suffix=".md")(compute, -1)
+Pipe(df.index, "data/project_cards/individual", output_suffix=".md", prefilter=False)(compute, 1)
 # exit()
